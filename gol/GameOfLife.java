9b5b973b53813c9b51e298a6b6a7787b95dbcb7f
DhruvP00012 <pandadhruv52@gmail.com>
2025-12-09 22:11:31 +0000

    finished more

--- gol/GameOfLife.java
+++ gol/GameOfLife.java
@@ -6,10 +6,9 @@ public class GameOfLife implements Board {
 
     private int[][] board;
 
-    public GameOfLife(int x, int y)
-    {
+    public GameOfLife(int x, int y) {
         this.board = new int[x][y];
-        for(int[] row : board) {
+        for (int[] row : board) {
             Arrays.fill(row, 0);
         }
     }
@@ -30,12 +29,30 @@ public class GameOfLife implements Board {
         }
     }
 
-    public void step()
-    {
+    public void step() {
         print();
-        // Update the game board, store a 1 if the cell is alive and a 0 otherwise.
-    }
+        int[][] nextboard = new int[board.length][board.length];
+        for (int x = 0; x < board.length; x++) {
+            for (int y = 0; y < board.length; y++) {
+                int aliveNeighbors = countNeighbors(x, y);
 
+                if (board[x][y] == 1) {
+                    if (aliveNeighbors < 2 || aliveNeighbors > 3) {
+                        nextboard[x][y] = 0;
+                    } else {
+                        nextboard[x][y] = 1;
+                    }
+                } else {
+                    if (aliveNeighbors == 3) {
+                        nextboard[x][y] = 1;
+                    } else {
+                        nextboard[x][y] = 0;
+                    }
+                }
+            }
+        }
+        this.board = nextboard;
+    }
 
     public int countNeighbors(int x, int y) {
         int count = 0;
@@ -49,34 +66,29 @@ public class GameOfLife implements Board {
     // Ex: -1 will read board.length-1
     public int get(int x, int y) {
         int xLimit = board.length;
-        int yLimit= board[0].length;
-        return board[(x+xLimit)%xLimit][(y+yLimit)%yLimit];
+        int yLimit = board[0].length;
+        return board[(x + xLimit) % xLimit][(y + yLimit) % yLimit];
     }
 
     // Test helper to get the whole board state
-    public int[][] get()
-    {
+    public int[][] get() {
         return board;
     }
 
     // Test helper to print the current state
-    public void print(){
+    public void print() {
         // Print the header
         System.out.print("\n ");
         for (int y = 0; y < board[0].length; y++) {
-            System.out.print(y%10 + " ");
+            System.out.print(y % 10 + " ");
         }
 
         for (int x = 0; x < board.length; x++) {
-            System.out.print("\n" + x%10);
-            for (int y=0; y<board[x].length; y++)
-            {
-                if (board[x][y] == 1)
-                {
+            System.out.print("\n" + x % 10);
+            for (int y = 0; y < board[x].length; y++) {
+                if (board[x][y] == 1) {
                     System.out.print("⬛");
-                }
-                else
-                {
+                } else {
                     System.out.print("⬜");
                 }
             }
