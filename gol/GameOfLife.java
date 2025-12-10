dab0866e95328483e073bf22a78d7a63b9afcae7
DhruvP00012 <pandadhruv52@gmail.com>
2025-12-10 03:25:14 +0000

    --- TEST PASSED! Congrats! --- (formatted cleaner + small tweaks)

--- gol/GameOfLife.java
+++ gol/GameOfLife.java
@@ -58,7 +58,8 @@ public class GameOfLife implements Board {
         int count = 0;
         for (int dx = -1; dx <= 1; dx++) {
             for (int dy = -1; dy <= 1; dy++) {
-                if (dx == 0 && dy == 0) continue; 
+                if (dx == 0 && dy == 0)
+                    continue;
 
                 count += get(x + dx, y + dy);
             }
