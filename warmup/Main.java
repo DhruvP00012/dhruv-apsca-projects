db87cadaf1b6c8845cd3ba939bb2ed305e76267c
DhruvP00012 <pandadhruv52@gmail.com>
2026-01-07 22:54:46 +0000

    Slight changes to project 1

--- warmup/Main.java
+++ warmup/Main.java
@@ -3,18 +3,21 @@ package warmup;
 public class Main {
 
     public static void main(String[] args) {
-        int sum = 0;
-        for(int i = 0; i < 1000; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                sum+=i;
-            }else if (i % 3 == 0 && i % 5 != 0) {
-                sum+=i;
-            }else if (i %3 != 0 && i % 5 == 0) {
-                sum+=i;
-            }else {
-                sum = sum + 0;
-            }
-        }
-        System.out.print(sum);
+           int sum1 = 0;
+           int sum2 = 0;
+           int sum3 = 0;
+           int sum4 = 0;
+           for(int i = 0; i < 1000; i++) {
+               if (i % 3 == 0 && i % 5 == 0) {
+                   sum1 += i;
+               }else if (i % 3 == 0 && i % 5 != 0) {
+                   sum2 += i;
+               }else if (i %3 != 0 && i % 5 == 0) {
+                   sum3 += i;
+               }else {
+                   sum4 = sum4 + 0;
+               }
+           }
+        System.out.print(sum1 + sum2 + sum3 +sum4);
     }
 }
