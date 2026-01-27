446ff9363da4343705c62a0c7b207fd4760d5bee
DhruvP00012 <pandadhruv52@gmail.com>
2026-01-27 21:56:09 +0000

    started insertion

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -4,5 +4,16 @@ public class InsertionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Insertion Sort!!!");
+        int n = input.length;
+
+        for(int i = 1; i < n; i++) {
+            int current = input[i];
+            int j;
+
+            for(j = i + 1; j >= 0 && input[j] > current; j--) {
+                input[j + 1] = input[j];
+            }
+            input[j + 1] = current;
+        }
     }
 }
