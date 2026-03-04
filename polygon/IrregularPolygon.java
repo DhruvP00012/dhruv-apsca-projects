c8d1be609da07dc47cef83b097fdf5dd5c4b0225
DhruvP00012 <pandadhruv52@gmail.com>
2026-03-04 20:19:20 +0000

    passed 1 test but failed rest(will fix)

--- polygon/IrregularPolygon.java
+++ polygon/IrregularPolygon.java
@@ -1,5 +1,6 @@
 package polygon;
 
+import java.awt.Point;
 import java.awt.Polygon;
 import java.awt.geom.*;
 import java.util.ArrayList;
@@ -35,7 +36,7 @@ public class IrregularPolygon {
         double sum = 0.0;
         for (int i = 0; i < myPolygon.size(); i++) {
             Point2D.Double one = myPolygon.get(i);
-            Point2D.Double two = myPolygon.get((i + 1) % myPolygon.size();
+            Point2D.Double two = myPolygon.get((i + 1) % myPolygon.size());
             sum = sum + (one.getX() * two.getX() - one.getY() * two.getY());
         }
         return Math.abs(sum / 2.0);
@@ -43,12 +44,20 @@ public class IrregularPolygon {
 
     public void draw()
     {
-        // Wrap the DrawingTool in a try/catch to allow development without need for graphics.
+        if (myPolygon.size() < 2) {
+            return;
+        }
         try {
-            // TODO: Draw the polygon.
-            // Documents: https://pavao.org/compsci/gpdraw/html/gpdraw/DrawingTool.html
-            //DrawingTool myDrawingTool = new DrawingTool(new SketchPad(500, 500));
-            //myDrawingTool.move(50, 50);
+            DrawingTool pencil = new DrawingTool(new SketchPad(500, 500));
+            Point2D.Double first = myPolygon.get(0);
+            pencil.up();
+            pencil.move(first.getX(), first.getY());
+            pencil.down();
+            for (int i = 1; i < myPolygon.size(); i++) {
+                Point2D.Double poly = myPolygon.get(i);
+                pencil.move(poly.getX(), poly.getY());
+            }
+            pencil.move(first.getX(), first.getY());
         } catch (java.awt.HeadlessException e) {
             System.out.println("Exception: No graphics support available.");
         }
