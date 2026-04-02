960721ae64046f60766222650037d9a54b8da8e3
DhruvP00012 <pandadhruv52@gmail.com>
2026-04-02 16:55:04 +0000

    progress with help (got idea) but test failed

--- tower/TowerSolver.java
+++ tower/TowerSolver.java
@@ -3,20 +3,21 @@ package tower;
 public class TowerSolver {
     private TowerModel model;
 
-    public TowerSolver()
-    {
+    public TowerSolver() {
     }
 
-    public void solve(TowerModel model)
-    {
+    public void solve(TowerModel model) {
         this.model = model;
         solve(model.getHeight(), model.getTowers()[0], model.getTowers()[1], model.getTowers()[2]);
     }
 
-    // Create an overloaded solve(...) method
-    // This new method will be recursive (call itself)
-    //
-    // [ solve method here]
-    //
-
+    private void solve(int n, IntegerStack from, IntegerStack to, IntegerStack aux) {
+        if (n == 1) {
+            to.push(from.pop());
+        } else {
+            solve(n - 1, from, aux, to);
+            to.push(from.pop());
+            solve(n - 1, aux, to, from);
+        }
+    }
 }
