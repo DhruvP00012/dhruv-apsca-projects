b00ac89fd21ff7a4173fab90ee494285a0653321
DhruvP00012 <pandadhruv52@gmail.com>
2026-04-03 00:04:06 +0000

    removed comments and still trying to fix code

--- tower/TowerModel.java
+++ tower/TowerModel.java
@@ -2,28 +2,11 @@ package tower;
 
 public class TowerModel {
 
-    // 2d array storing game state
     private IntegerStack[] towers;
-
-    // Final tower height
     private int towerHeight = 0;
-
-    // Debug metrics
     private int printCounter = 0;
     private int moveCounter = 0;
 
-    /* This class implements a model of a tower of Hanoi game.
-
-        |    |    |
-        =    |    |
-       ===   |    |
-      =====  |    |
-     ------------------
-        0    1    2
-
-     Example of a game of height three in the starting position.
-
-    */
     public TowerModel(int height)
     {
         towerHeight = height;
@@ -31,32 +14,27 @@ public class TowerModel {
         towers[0] = new IntegerStack(height);
         towers[1] = new IntegerStack(height);
         towers[2] = new IntegerStack(height);
-
         for (int i=0; i<height; i++)
         {
             towers[0].push(height - i);
         }
     }
 
-    // get the total number of disks
     public int height()
     {
         return towerHeight;
     }
 
-
-    // Move one disk from the source stack to the destination stack.
     public void move(int source, int destination)
     {
+        int disk = towers[source].pop();
+        towers[destination].push(disk);
         System.out.println("Move #" + ++moveCounter + " from " + source + " to " + destination);
-        // TODO!!
     }
 
-    // Helper method to nicely print the current model state.
     public void print()
     {
         System.out.print("Print #" + ++printCounter + " Towers of Hanoi\n");
-
         for (int layer = towerHeight-1; layer >= 0; layer--){
             System.out.print("\n");
             for (int tower = 0; tower < towers.length; tower++) {
@@ -77,7 +55,6 @@ public class TowerModel {
         System.out.println();
     }
 
-    // Test instrumentation
     public IntegerStack[] getTowers()
     {
         return towers;
--- tower/TowerSolver.java
+++ tower/TowerSolver.java
@@ -8,15 +8,15 @@ public class TowerSolver {
 
     public void solve(TowerModel model) {
         this.model = model;
-        solve(model.getHeight(), model.getTowers()[0], model.getTowers()[2], model.getTowers()[1]);
+        solve(model.getHeight(), 0, 2, 1);
     }
 
-    private void solve(int n, IntegerStack from, IntegerStack to, IntegerStack aux) {
+    private void solve(int n, int from, int to, int aux) {
         if (n == 1) {
-            to.push(from.pop());
+            model.move(from, to);
         } else {
             solve(n - 1, from, aux, to);
-            to.push(from.pop());
+            model.move(from, to);
             solve(n - 1, aux, to, from);
         }
     }
